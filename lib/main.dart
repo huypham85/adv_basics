8a4a4bf63a4fe88da26454365fab509eee5c8f7a
HuyPT3 <huypt3@rikkeisoft.com>
2024-03-19 18:19:51 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:adv_basics/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    ),
+  );
+}
